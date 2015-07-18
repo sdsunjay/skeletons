76f4fb67e058814927df27dc4ddc1d86f8f5f58f
Sunjay Dhama <sdsunjay73@yahoo.com>
2015-07-18 00:10:50 -0700

    google script for installing pagespeed module, taken from https://developers.google.com/speed/pagespeed/module/build_ngx_pagespeed_from_source

---
+++ centos/google_pagespeed_script.sh
@@ -0,0 +1,7 @@
+cd
+NPS_VERSION=1.9.32.4
+wget https://github.com/pagespeed/ngx_pagespeed/archive/release-${NPS_VERSION}-beta.zip
+unzip release-${NPS_VERSION}-beta.zip
+cd ngx_pagespeed-release-${NPS_VERSION}-beta/
+wget https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz
+tar -xzvf ${NPS_VERSION}.tar.gz  # extracts to psol/
