53068bf5b79ee2855bb857f9061f79024a32f97b
Sunjay Dhama <sdsunjay73@yahoo.com>
2021-03-01 04:06:25 +0000

    new one

--- centos/iptables.sh
+++ centos/iptables.sh
@@ -6,12 +6,11 @@
 # http://www.thegeekstuff.com/scripts/iptables-rules
 # http://www.rackspace.com/knowledge_center/article/mail-server-opening-ports-in-the-linux-firewall
 # Path to block list
-_input=/some/path/iptables/blockList.txt
+_input=/home/deploy/blockList.txt
 # interface
-_pub_if="venet0"
+_pub_if="eth0"
 # Will differ depending on your system
 IPT=/sbin/iptables
-
 # Die if file not found
 [ ! -f "$_input" ] && { echo "$0: File $_input not found."; exit 1; }
 
@@ -39,81 +38,92 @@ $IPT -A INPUT -i ${_pub_if} -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 ### Setup our black list ###
 # Create a new chain 
-$IPT -N blockList
-echo starting to read: $_input
+# $IPT -N droplist
+ 
 # Filter out comments and blank lines
 # store each ip or subnet in $ip
-while IFS= read -r ip
-do
+# while IFS= read -r ip
+#do
         # Append everything to droplist
-   $IPT -A blockList -i ${_pub_if} -s $ip -j LOG --log-prefix " Drop Bad IP List "
-   $IPT -A blockList -i ${_pub_if} -s $ip -j DROP
-done <"${_input}"
+#	$IPT -A droplist -i ${_pub_if} -s $ip -j LOG --log-prefix " Drop Bad IP List "
+#	$IPT -A droplist -i ${_pub_if} -s $ip -j DROP
+# done <"${_input}"
  
 # Finally, insert or append our black list 
-$IPT -I INPUT -j blockList
-$IPT -I OUTPUT -j blockList
-$IPT -I FORWARD -j blockList
-
+# $IPT -I INPUT -j droplist
+# $IPT -I OUTPUT -j droplist
+# $IPT -I FORWARD -j droplist
 
 # Okay add your rest of $IPT commands here 
 # Example: open port 53
 #$IPT -A INPUT -i ${_pub_if} -s 0/0 -d 1.2.3.4 -p udp --dport 53 -j ACCEPT
 #$IPT -A INPUT -i ${_pub_if} -s 0/0 -d 1.2.3.4 -p tcp --dport 53 -j ACCEPT
 
-echo Open outgoing and incoming port 80 for web
+echo Open outgoing and incoming port 80 and 443 for web
 # Open port 80 for web
 #http
 $IPT -A INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT
 #https
 $IPT -A INPUT -m state --state NEW -p tcp --dport 443 -j ACCEPT
 
-
 echo Open incoming port 22 for ssh
 # Open port 22 for ssh
 $IPT -A INPUT -p tcp --dport 22 -j ACCEPT
 $IPT -A OUTPUT -p tcp --sport 22 -j ACCEPT
 
-echo Open port 25 for sendmail
+echo Open incoming port 9090 for cockpit
+# Open port 9090 for cockpit
+$IPT -A INPUT -p tcp --dport 9090 -j ACCEPT
+$IPT -A OUTPUT -p tcp --sport 9090 -j ACCEPT
+
+# echo Open port 25 for sendmail
 # Allow Sendmail or Postfix
-$IPT -A INPUT -p tcp --dport 25 -j ACCEPT
-$IPT -A OUTPUT -p tcp --sport 25 -j ACCEPT
+# $IPT -A INPUT -p tcp --dport 25 -j ACCEPT
+# $IPT -A OUTPUT -p tcp --sport 25 -j ACCEPT
 
 echo Open port 465 for secure sendmail
 # Allow Sendmail or Postfix
 $IPT -A INPUT -p tcp --dport 465 -j ACCEPT
 $IPT -A OUTPUT -p tcp --sport 465 -j ACCEPT
 
+echo Open port 587 for sendgrid
+$IPT -A INPUT -p tcp --dport 587 -j ACCEPT
+$IPT -A OUTPUT -p tcp --sport 587 -j ACCEPT
+
 echo Allow incoming ICMP ping pong stuff
  # Allow incoming ICMP ping pong stuff
-$IPT -A INPUT -i ${_pub_if} -p icmp --icmp-type 8 -m state --state NEW,ESTABLISHED,RELATED -m limit --limit 30/sec  -j ACCEPT
-$IPT -A INPUT -i ${_pub_if}  -p icmp -m icmp --icmp-type 3 -m limit --limit 30/sec -j ACCEPT
-$IPT -A INPUT -i ${_pub_if}  -p icmp -m icmp --icmp-type 5 -m limit --limit 30/sec -j ACCEPT
-$IPT -A INPUT -i ${_pub_if}  -p icmp -m icmp --icmp-type 11 -m limit --limit 30/sec -j ACCEPT
+$IPT -A INPUT -p icmp --icmp-type 8 -m state --state NEW,ESTABLISHED,RELATED -m limit --limit 30/sec  -j ACCEPT
+$IPT -A INPUT -p icmp -m icmp --icmp-type 3 -m limit --limit 30/sec -j ACCEPT
+$IPT -A INPUT -p icmp -m icmp --icmp-type 5 -m limit --limit 30/sec -j ACCEPT
+$IPT -A INPUT -p icmp -m icmp --icmp-type 11 -m limit --limit 30/sec -j ACCEPT
  
 echo Allow outbound DNS
 # Allow outbound DNS
-$IPT -A OUTPUT -p udp -o ${_pub_if} --dport 53 -j ACCEPT
-$IPT -A INPUT -p udp -i ${_pub_if} --sport 53 -j ACCEPT
+$IPT -A OUTPUT -p udp --dport 53 -j ACCEPT
+$IPT -A INPUT -p udp --sport 53 -j ACCEPT
 
 # Security 
 # Syn-flood protection:
-$IPT -A FORWARD -p tcp --syn -m limit --limit 1/s -j ACCEPT
+# $IPT -A FORWARD -p tcp --syn -m limit --limit 1/s -j ACCEPT
 # Furtive port scanner:
-$IPT -A FORWARD -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s -j ACCEPT
+# $IPT -A FORWARD -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s -j ACCEPT
 # Ping of death:
+echo Blocking ping of death
 $IPT -A FORWARD -p icmp --icmp-type echo-request -m limit --limit 1/s -j ACCEPT
 # Prevent DoS attack
-$IPT -A INPUT -p tcp --dport 80 -m limit --limit 25/minute --limit-burst 100 -j ACCEPT
-
+# $IPT -A INPUT -p tcp --dport 80 -m limit --limit 25/minute --limit-burst 100 -j ACCEPT
+echo Allow docker
+$IPT -A FORWARD -i docker0 -o eth0 -j ACCEPT
+$IPT -A FORWARD -i eth0 -o docker0 -j ACCEPT
 echo drop and log everything else
 # drop and log everything else
 # limiting the number of times we log
 # Log dropped packets
 $IPT -N LOGGING
 $IPT -A INPUT -j LOGGING
-$IPT -A LOGGING -m limit --limit 5/min --limit-burst 7 -j LOG --log-prefix "IPtables Packet Dropped: " --log-level 5
+$IPT -A LOGGING -m limit --limit 25/min --limit-burst 50 -j LOG --log-prefix "IPtables Packet Dropped: " --log-level 5
 $IPT -A LOGGING -j DROP
 
 #save IP tables rules to a file
-iptables-save > /root/usefulScripts/iptables/iptables_saved
+echo iptables-save 
+iptables-save > /home/deploy/skeletons/usefulScripts/iptables_saved
