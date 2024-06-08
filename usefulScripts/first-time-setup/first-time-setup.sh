e0271a41de756b4b8f23d337ce765496639cf3ce
Sunjay Dhama <sdsunjay@users.noreply.github.com>
2024-06-08 07:54:15 -0400

    Refactored first-time-setup.sh for improved readability and functionality
    - Change shebang from #!/bin/bash to #!/bin/sh for broader compatibility.
    - Introduce run_command function to echo and run commands, enhancing script clarity.
    - Rename 'pattern' variable to 'first_line' for better context in add_block_to_zshrc function.
    - Add 'echo' statements to log actions like adding blocks to `.zshrc` for improved user feedback.
    - Wrap critical commands with run_command for consistent command execution and logging.
    - Use regex for case-insensitive user input matching in prompts, improving user experience.
    - Refactor SSH key generation prompt to use prompt_user_input function for consistency.
    - Replace exec zsh with run_command source $ZSHRC_FILE to properly reload shell.
    - Add prompt for optional installation of Node.js, npm, TypeScript, and AWS CLI.
    - Add installation prompts for 'little-snitch' and 'python-autopep8' packages.

--- usefulScripts/first-time-setup/first-time-setup.sh
+++ usefulScripts/first-time-setup/first-time-setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Global variables
 GITHUB_VUNDLE_REPO="https://github.com/gmarik/Vundle.vim.git"
@@ -9,6 +9,12 @@ ZSHRC_FILE="$HOME/.zshrc"
 error_exit() {
     printf "Error: %s\n" "$1" >&2
     exit 1
+}
+
+# Function to echo and run commands
+run_command() {
+    echo "$@"
+    "$@"
 }
 
 # Function to prompt user input with validation
@@ -76,7 +82,7 @@ prompt_optional_cask_pkg() {
 # Function to add blocks of commands to .zshrc
 add_block_to_zshrc() {
     local block="$1"
-    local pattern=$(echo "$block" | head -n 1) # Use the first line of the block as a pattern
+    local first_line=$(echo "$block" | head -n 1) # Use the first line of the block as a pattern
 
     # Check if .zshrc exists, create if it does not
     if [[ ! -f "$ZSHRC_FILE" ]]; then
@@ -84,8 +90,11 @@ add_block_to_zshrc() {
     fi
 
     # Append block if the pattern is not already in the file
-    if ! grep -qF "$pattern" "$ZSHRC_FILE"; then
+    if ! grep -qF "$first_line" "$ZSHRC_FILE"; then
         printf "\n%s\n" "$block" >> "$ZSHRC_FILE"
+        echo "Block added to $ZSHRC_FILE"
+    else
+        echo "Block already exists in $ZSHRC_FILE"
     fi
 }
 
@@ -94,7 +103,7 @@ install_xcode_command_line_tools() {
     # Check if Xcode Command Line Tools are installed
     if ! xcode-select --print-path &>/dev/null; then
         # Attempt to install Xcode Command Line Tools
-        xcode-select --install && return 0
+        run_command xcode-select --install && return 0
         error_exit "Failed to install Xcode command line tools"
     else
         printf "Xcode Command Line Tools already installed.\n"
@@ -104,14 +113,10 @@ install_xcode_command_line_tools() {
 # Function to ask user to install Python
 prompt_user_to_select_install_pyenv(){
     # Prompt the user for confirmation
-    read -p "Do you want to select and install a Python3 version using pyenv? (y/n): " user_input
-
-    # Normalize the input to lowercase to handle cases like 'Y' or 'N'
-    user_input=${user_input,,}  # Converts to lowercase
-
-    if [[ "$user_input" == "y" ]]; then
+    read -p "Do you want to select and install a Python3 version using pyenv? It is highly recommended! (y/n): " response
+    if [[ "$response" =~ ^[Yy]$ ]]; then
         # Execute the script to select and install Python version
-	    bash select-install-pyenv.sh
+	    run_command ./select-install-pyenv.sh
     else
 	    echo "You selected not to select and install a version of python."
     fi
@@ -121,14 +126,11 @@ prompt_user_to_select_install_pyenv(){
 # Function to prompt user to install Oh My ZSH!
 prompt_user_to_install_oh_my_zsh(){
     # Prompt the user for confirmation
-    read -p "Do you want to install a Oh My ZSH? (y/n): " user_input
+    read -p "Do you want to install a Oh My ZSH? It is highly recommended! (y/n): " response
 
-    # Normalize the input to lowercase to handle cases like 'Y' or 'N'
-    user_input=${user_input,,}  # Converts to lowercase
-
-    if [[ "$user_input" == "y" ]]; then
+    if [[ "$response" =~ ^[Yy]$ ]]; then
         # Execute the script to select and install Python version
-	    bash install-oh-my-zsh.sh
+	    run_command ./install-oh-my-zsh.sh
     else
 	    echo "You selected not install Oh My ZSH!."
     fi
@@ -141,15 +143,12 @@ install_iterm2_shell_integration() {
         echo "iTerm2 is installed."
 
         # Ask the user if they want to install iTerm2 shell integrations
-        read -p "Do you want to install iTerm2 shell integrations? (y/n): " user_input
+        read -p "Do you want to install iTerm2 shell integrations? (y/n): " response
 
-        # Normalize the input to lowercase
-        user_input=${user_input,,}  # Converts to lowercase
-
-        if [[ "$user_input" == "y" ]]; then
+        if [[ "$response" =~ ^[Yy]$ ]]; then
             # If the user agrees, run the iTerm2 shell integrations installation script
             echo "Installing iTerm2 shell integrations..."
-            curl -L https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
+            run_command curl -L https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
         else
             echo "Installation cancelled by user."
         fi
@@ -158,36 +157,66 @@ install_iterm2_shell_integration() {
     fi
 }
 
+# Define the blocks of code for zsh-completions
+read -r -d '' BREW_COMPINIT_BLOCK << 'EOF'
+if type brew &>/dev/null; then
+    FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
 
-# Define the blocks of code
-# for zsh-completions
-BREW_COMPINIT_BLOCK="if type brew &>/dev/null; then
-    FPATH=\$(brew --prefix)/share/zsh-completions:\$FPATH
-    autoload -Uz compinit
-    compinit
-fi"
-
-# for pyenv
-PYENV_BLOCK="eval \"\$(pyenv init -)\"
-if which pyenv-virtualenv-init > /dev/null; then
-    eval \"\$(pyenv virtualenv-init -)\"
-fi"
+    # Only run compinit if it hasn't been run already in this session
+    if ! type compinit &>/dev/null; then
+        autoload -Uz compinit
+        compinit
+    fi
+fi
+EOF
 
 system_init() {
-    sudo systemsetup -gettimezone # ask the user if they want to set the timezone
-    sudo systemsetup -setremotelogin off
-    sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
-    sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on
 
-    sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate -stop
-    sudo launchctl print system/com.apple.remoted
-    sudo launchctl disable system/com.apple.remoted
-    sudo launchctl bootout system/com.apple.remoted
+    run_command sudo systemsetup -setremotelogin off
+    run_command sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+    run_command sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on
+
+    # Invert direction of mouse scroll
+    run_command defaults write -g com.apple.swipescrolldirection -bool false
+
+    # Move the dock to the left and make it smaller
+    run_command defaults write com.apple.dock orientation -string left
+    run_command defaults write com.apple.dock tilesize -int 30
+    run_command killall Dock
+
+    # Prompt the user for confirmation
+    read -p "Do you want to disable remoted from starting using 'launchctl' (y/n): " response
+
+    if [[ "$response" =~ ^[Yy]$ ]]; then
+        run_command sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate -stop
+        run_command sudo launchctl print system/com.apple.remoted
+        run_command sudo launchctl disable system/com.apple.remoted
+        run_command sudo launchctl bootout system/com.apple.remoted
+    else
+	    echo "You selected not disable remoted"
+    fi
+
+    # Prompt the user for confirmation
+    read -p "Do you want to completely disable Siri? System restart is required. (y/n): " response
+
+    if [[ "$response" =~ ^[Yy]$ ]]; then
+        run_command ./disable-siri.sh
+    else
+	    echo "You selected not disable Siri"
+    fi
+
 
 }
 
 # Main function
 main() {
+
+    # Welcome message
+    echo "Welcome! This script will configure various settings and install needed software for you."
+
+    # System changes for a new OS
+    system_init
+
     printf "This script will help you set up your new Apple computer\n"
     # Install Xcode command line tools
     install_xcode_command_line_tools
@@ -199,8 +228,8 @@ main() {
     fi
 
     # Update and upgrade brew
-    brew update || error_exit "Failed to update Homebrew"
-    brew upgrade || error_exit "Failed to upgrade Homebrew"
+    run_command brew update || error_exit "Failed to update Homebrew"
+    run_command brew upgrade || error_exit "Failed to upgrade Homebrew"
 
     # Install essential brew packages
     install_brew_pkg "bash"
@@ -212,24 +241,20 @@ main() {
     install_brew_pkg "ruby"
     install_brew_pkg "pyenv"
     install_brew_pkg "pyenv-virtualenv"
-    brew postinstall python3
+    run_command brew postinstall python3
     # Set permissions for zsh-completions
     printf "Setting permissions for zsh-completions... \n"
-    chmod go-w '/usr/local/share'
-    chmod -R go-w '/usr/local/share/zsh'
+    run_command chmod go-w '/usr/local/share'
+    run_command chmod -R go-w '/usr/local/share/zsh'
 
-    # Add blocks to .zshrc
+    # Add zsh-completions block to .zshrc
     echo "Adding zsh-completions to .zshrc"
     add_block_to_zshrc "$BREW_COMPINIT_BLOCK"
-    echo "Adding pyenv to .zshrc"
-    add_block_to_zshrc "$PYENV_BLOCK"
-
     echo "Updates to .zshrc completed."
-    source ~/.zshrc
 
-
+    # Source the .zshrc to apply changes
+    run_command source "$ZSHRC_FILE"
     prompt_user_to_select_install_pyenv
-
 
     # Install optional brew packages
     prompt_optional_pkg "nvm"
@@ -239,6 +264,7 @@ main() {
     prompt_optional_pkg "cowsay"
     prompt_optional_pkg "speedtest-cli"
     prompt_optional_pkg "python"
+    prompt_optional_pkg "python-autopep8"
     prompt_optional_pkg "sqlite"
     prompt_optional_pkg "mysql"
     prompt_optional_pkg "postgresql"
@@ -249,15 +275,17 @@ main() {
     prompt_optional_pkg "jq"
     prompt_optional_pkg "nmap"
 
-    # Prompt for optional cask packages
+    # install iterm2
     install_cask_pkg "iterm2"
     # Call the function to execute the installation process
     install_iterm2_shell_integration
 
+    # Prompt for optional cask packages
     prompt_optional_cask_pkg "brave-browser"
     prompt_optional_cask_pkg "knockknock"
     prompt_optional_cask_pkg "intellij-idea-ce"
     prompt_optional_cask_pkg "docker"
+    prompt_optional_cask_pkg "little-snitch"
     prompt_optional_cask_pkg "java"
     prompt_optional_cask_pkg "spotify"
     prompt_optional_cask_pkg "google-chrome"
@@ -272,8 +300,9 @@ main() {
     if ! git clone "$GITHUB_VUNDLE_REPO" "$VUNDLE_DIR"; then
         error_exit "Failed to clone Vundle.vim"
     fi
+
     # install vundle plugins
-    /usr/local/bin/vim +PluginInstall +qall
+    run_command /usr/local/bin/vim +PluginInstall +qall
 
     # Clean up outdated brew packages
     printf "Cleaning up outdated brew packages...\n"
@@ -284,9 +313,9 @@ main() {
 
     # Configure Git
     # Ask the user if they want to install iTerm2 shell integrations
-    read -p "Do you want to configure git? (y/n): " user_input
+    read -p "Do you want to configure git? (y/n): " response
 
-    if [[ "$user_input" == "y" ]]; then
+    if [[ "$response" =~ ^[Yy]$ ]]; then
         # If the user agrees, run the git config commands
         printf "Configuring Git...\n"
         local name email comment
@@ -297,9 +326,9 @@ main() {
         git config --global core.editor vim
     fi
 
-    read -p "Do you want to generate an ssh key? (y/n): " user_input
+    response=$(prompt_user_input 'Do you want to generate an ssh key? (y/n): ')
 
-    if [[ "$user_input" == "y" ]]; then
+    if [[ "$response" =~ ^[Yy]$ ]]; then
       # Generate SSH key
       comment=$(prompt_user_input 'Type your comment for SSH key, followed by [ENTER]: ')
       printf "Generating new SSH key...\n"
@@ -325,7 +354,17 @@ main() {
     fi
 
     # Reload the shell to apply `.zshrc` changes
-    exec zsh
+    run_command source $ZSHRC_FILE
+
+    # Prompt the user for confirmation
+    read -p "Do you want to install node, npm, typescript, aws-cli. It is highly recommended! (y/n): " response
+
+    if [[ "$response" =~ ^[Yy]$ ]]; then
+        echo "now running node-aws-cdk-setup.sh"
+        run_command ./node-aws-cdk-setup.sh
+    else
+	    echo "You selected not install node, npm, typescript, nor aws-cli."
+    fi
 }
 
 # Execute main function
